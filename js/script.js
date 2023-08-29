34ae74db0e1865210510eab43731c894fb09370e
LucasSSLV <lukinhas.kell@hotmail.com>
2023-08-29 19:10:04 -0300

    atualizações nos inputs

--- js/script.js
+++ js/script.js
@@ -10,29 +10,38 @@ function escopoGeral() {
     const valorPeso = document.querySelector(".peso");
     const valorAltura = document.querySelector(".altura");
 
+    function limpaCampos() {
+      valorPeso.value = "";
+      valorAltura.value = "";
+    }
+
     const imcFormatado = (
       valorPeso.value /
       (valorAltura.value * valorAltura.value)
     ).toFixed(1);
 
-    valorPeso.value = "";
-    valorAltura.value = "";
-
     let tagResposta =
       (resultado.innerHTML = `<p>Seu IMC é de: ${imcFormatado}</p>`);
     if (valorPeso.value === "" || valorAltura.value === "") {
+      limpaCampos();
       return (resultado.innerHTML = `<p>Valores Inválidos!</p>`);
     } else if ((imcFormatado < 18, 5)) {
+      limpaCampos();
       return tagResposta;
     } else if ((imcFormatado >= 18, 5 || imcFormatado >= 24, 9)) {
+      limpaCampos();
       return tagResposta;
     } else if ((imcFormatado >= 25 || imcFormatado <= 29, 9)) {
+      limpaCampos();
       return tagResposta;
     } else if ((imcFormatado >= 30 || imcFormatado <= 34, 9)) {
+      limpaCampos();
       return tagResposta;
     } else if ((imcFormatado >= 35 || imcFormatado <= 39, 9)) {
+      limpaCampos();
       return tagResposta;
     } else if (imcFormatado >= 40) {
+      limpaCampos();
       return tagResposta;
     }
   }
